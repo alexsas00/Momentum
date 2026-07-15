818f5745d2896ab31949b26175f4284e611dc994
alexsas00 <160885341+alexsas00@users.noreply.github.com>
2026-07-15 14:46:08 +0200

    Update RootView.swift

--- Sources/App/RootView.swift
+++ Sources/App/RootView.swift
@@ -10,11 +10,15 @@ struct RootView: View {
     // On iOS 26 the tab bar is Liquid Glass automatically (built with the 26 SDK);
     // we additionally let it recede while scrolling for a calmer canvas.
     @ViewBuilder private var tabs: some View {
+        #if compiler(>=6.2)
         if #available(iOS 26, *) {
             baseTabs.tabBarMinimizeBehavior(.onScrollDown)
         } else {
             baseTabs
         }
+        #else
+        baseTabs
+        #endif
     }
 
     private var baseTabs: some View {
